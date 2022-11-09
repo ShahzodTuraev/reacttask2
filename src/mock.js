3dda9d1671165fd79f8c8f2367e205fbd90a538b
ShahzodTuraev <turayevshahzodbek@gmail.com>
2022-11-09 18:28:33 +0900

    It is not ready fully

---
+++ src/mock.js
@@ -0,0 +1,8 @@
+export const student = [
+    {id: 1, name: 'Shahzod', address: 'Uzbekistan', status: 'student'},
+    {id: 2, name: 'Shaxboz', address: 'Uzbekistan', status: 'student'},
+    {id: 3, name: 'Nodir', address: 'Uzbekistan', status: 'student'},
+    {id: 4, name: 'Bobur', address: 'Uzbekistan', status: 'student'},
+    {id: 5, name: 'Sobir', address: 'Uzbekistan', status: 'student'},
+    {id: 6, name: 'Ali', address: 'Uzbekistan', status: 'student'},
+]
