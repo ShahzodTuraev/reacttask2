2b2dd45f34a51f135da822b52d2f9065eed1b028
ShahzodTuraev <turayevshahzodbek@gmail.com>
2022-11-19 15:06:57 +0900

    fully CRUD functions in React

--- src/mock.js
+++ src/mock.js
@@ -1,8 +1,13 @@
-export const student = [
-    {id: 1, name: 'Shahzod', address: 'Uzbekistan', status: 'student'},
-    {id: 2, name: 'Shaxboz', address: 'Uzbekistan', status: 'student'},
-    {id: 3, name: 'Nodir', address: 'Uzbekistan', status: 'student'},
-    {id: 4, name: 'Bobur', address: 'Uzbekistan', status: 'student'},
-    {id: 5, name: 'Sobir', address: 'Uzbekistan', status: 'student'},
-    {id: 6, name: 'Ali', address: 'Uzbekistan', status: 'student'},
+export const students = [
+    {id: 1, name: 'Shahzod', address: 'Uzbekistan', age:'25', university: 'Dong-A University', status: 'student'},
+    {id: 2, name: 'Shaxboz', address: 'Pakistan', age:'27', university: 'Dong-A University', status: 'developer'},
+    {id: 3, name: 'Nodir', address: 'Uzbekistan', age:'21', university: 'Dong-A University', status: 'doctor'},
+    {id: 4, name: 'Bobur', address: 'USA', age:'35', university: 'Dong-A University', status: 'student'},
+    {id: 5, name: 'Sobir', address: 'Korea', age:'44', university: 'Dong-A University', status: 'dentist'},
+    {id: 6, name: 'Ali', address: 'Japan', age:'36', university: 'Dong-A University', status: 'manager'},
+    {id: 7, name: 'Tim', address: 'England', age:'32', university: 'Dong-A University', status: 'engineer'},
+    {id: 8, name: 'Jeck', address: 'Uzbekistan', age:'29', university: 'Dong-A University', status: 'constructor'},
+    {id: 9, name: 'Stive', address: 'Utopia', age:'24', university: 'Dong-A University', status: 'teacher'},
+    {id: 10, name: 'Alexandr', address: 'Uzbekistan', university: 'Dong-A University', age:'39', status: 'guardian'},
+    {id: 11, name: 'Bob', address: 'Sealand', age:'40', university: 'Dong-A University', status: 'businessman'},
 ]
