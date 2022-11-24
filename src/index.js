29514ab17ba87610620df9471897205c58626642
ShahzodTuraev <turayevshahzodbek@gmail.com>
2022-11-24 22:55:16 +0900

    counter in hooks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import State from './state';
+import Hooks from './Hooks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <State />
+    <Hooks />
   </React.StrictMode>
 );
 
